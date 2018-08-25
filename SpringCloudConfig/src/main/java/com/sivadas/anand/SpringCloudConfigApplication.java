b809ec94d98b08dfc05473aafc6ed3180aa319d1
r82anand <r82anand@gmail.com>
2018-08-25 20:56:16 +0530

    initial checkin

---
+++ SpringCloudConfig/src/main/java/com/sivadas/anand/SpringCloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.sivadas.anand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+}
