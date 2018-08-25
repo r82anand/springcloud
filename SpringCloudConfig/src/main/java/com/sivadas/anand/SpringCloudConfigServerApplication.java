6b6d48bae84d92cb8bb2cd7786381a34bb132de3
r82anand <r82anand@gmail.com>
2018-08-25 21:19:50 +0530

    added docker config

--- SpringCloudConfig/src/main/java/com/sivadas/anand/SpringCloudConfigServerApplication.java
+++ SpringCloudConfig/src/main/java/com/sivadas/anand/SpringCloudConfigServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudConfigApplication {
+public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigApplication.class, args);
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 }
--- SpringCloudConfig/src/test/java/com/sivadas/anand/SpringCloudConfigServerApplicationTests.java
+++ SpringCloudConfig/src/test/java/com/sivadas/anand/SpringCloudConfigServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCloudConfigApplicationTests {
+public class SpringCloudConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
