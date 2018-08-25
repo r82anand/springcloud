c6ea08c3012cd1eedf50f4656d1e33bed0a582b6
r82anand <r82anand@gmail.com>
2018-08-25 22:55:33 +0530

    added client project

---
+++ SpringCloudConfigClient/src/main/java/com/sivadas/anand/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.sivadas.anand.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@Value("${greeting.message}")
+	private String message;
+	
+	@GetMapping(value="/hi")
+	public String sayHello() {
+		return message;
+	}
+	
+}
